d83e14f9434966bd514f06fb07718b27a07bda56
Komorio <twilivez@gmail.com>
2020-04-30 22:04:58 +0900

    [ADD] MonsterCreator 및 StageManager 추가
    00. StageManager에서는 MonsterCreateor 를 갖고 있으며 실정시간하마 Execute 함수를 호출 합니다.
    01. MonsterCreator의 Exeucte 함수에서는 Monster를 생성합니다.

---
+++ Assets/01.Scripts/Manager/StageManager/MonsterCreator.cs
@@ -0,0 +1,48 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterCreator : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject monstersParentObject;
+
+    private List<Monster> monsterList = new List<Monster>();
+
+    private Vector2 leftGeneratePosition;
+    private Vector2 rightGeneratePosition;
+
+    private void Awake(){
+        leftGeneratePosition = Vector2.zero;
+        rightGeneratePosition = Vector2.zero;
+
+        leftGeneratePosition.x = -10;
+        rightGeneratePosition.x = 10;
+
+        monsterList = monstersParentObject.GetComponentsInChildren<Monster>(true).ToList();
+    }
+
+    [ContextMenu("Monster Generate")]
+    public void Execute(){
+        Monster generatedMonster;
+
+        generatedMonster = GetAvailableMonster();
+        
+        generatedMonster.gameObject.transform.position = Random.Range(0,2).Equals(0)
+        ? leftGeneratePosition
+        : rightGeneratePosition;
+
+        generatedMonster.Execute();
+    }
+
+    private Monster GetAvailableMonster(){
+        for(int i = 0; i < monsterList.Count; i++){
+            if(monsterList[i].gameObject.activeInHierarchy.Equals(false)){
+                return monsterList[i];
+            }
+        }
+        return null;
+    }
+}
---
+++ Assets/01.Scripts/Manager/StageManager/StageManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageManager : MonoBehaviour
+{
+    private MonsterCreator monsterCreator;
+
+    [SerializeField]
+    private float monsterGenenrateInterval;
+
+    private void Awake(){
+        monsterCreator = gameObject.GetComponent<MonsterCreator>();
+    }
+
+    private void Start(){
+        StartCoroutine(MonsterGenerateCoroutine());
+    }
+
+    private IEnumerator MonsterGenerateCoroutine(){
+        while(true){
+            monsterCreator.Execute();
+            yield return YieldInstructionCache.WaitingSecond(monsterGenenrateInterval);
+        }
+    }
+
+}
