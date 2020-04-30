82869cf01f5b20c8062b9cc54c6e1206a35b3584
Komorio <twilivez@gmail.com>
2020-04-30 21:02:29 +0900

    [ADD] TouchManager 및 GameManager 추가

---
+++ Assets/01.Scripts/Manager/GameManager/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{  
+    public static GameManager instance;
+    
+    [HideInInspector]
+    public TouchManager touchManager;
+
+
+    private void Awake(){ 
+        if(instance is null){
+            instance = this;
+        }
+
+        touchManager = gameObject.GetComponent<TouchManager>();
+    } 
+}
---
+++ Assets/01.Scripts/Manager/GameManager/TouchManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchManager : MonoBehaviour
+{
+    private Vector2 touchDownPositionNotScreen;
+    
+    private Vector2 touchDownPosition;
+    private Vector2 touchUpPosition;
+
+    private Vector2 swipeDirection;
+
+    private float minSwipeDistance;
+
+    private bool isTouchDown;
+    private bool isTouchUp;
+    private bool isSwipe;
+
+    public Vector2 TouchDownPosition => touchDownPosition;
+    public Vector2 TouchUpPosition => touchUpPosition;
+    public Vector2 SwipeDirection => swipeDirection;
+
+    public bool IsTouchDown => isTouchDown;
+    public bool IsTouchUp => isTouchUp;
+    public bool IsSwipe => isSwipe;
+
+    private void Awake(){
+        minSwipeDistance = Screen.width / 5;
+    }
+
+    private void Update(){
+        ProcessingTouch();
+    }
+
+    private void ProcessingTouch(){
+        if(Input.touchCount > 0){
+            Touch tempTouch = Input.touches[0];
+
+            if(tempTouch.phase.Equals(TouchPhase.Began)){
+                touchDownPositionNotScreen = tempTouch.position;
+                touchDownPosition = Camera.main.ScreenToWorldPoint(touchDownPositionNotScreen);
+
+                isTouchDown = true;
+            }
+            else if(tempTouch.phase.Equals(TouchPhase.Moved)){
+                Vector2 currentPosition = tempTouch.position;
+
+                if((currentPosition - touchDownPositionNotScreen).magnitude > minSwipeDistance){
+                    swipeDirection = (currentPosition - touchDownPositionNotScreen).normalized;
+                }
+            }
+            else if(tempTouch.phase.Equals(TouchPhase.Ended)){
+                isTouchDown = false;
+                isSwipe = false;
+                isTouchUp = true;
+            }
+        }
+    }
+}
