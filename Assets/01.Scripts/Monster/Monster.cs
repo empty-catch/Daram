4f8cf1fdd1d259d488ed5e076762a41de0cb89b5
Komorio <twilivez@gmail.com>
2020-04-30 21:32:14 +0900

    [ADD] Monster Script 추가
    00. 현재는 중심을 향해 움직이는 기능만 갖고 있음

---
+++ Assets/01.Scripts/Monster/Monster.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+
+    private Vector2 moveDirection;
+
+
+    public void Execute(){
+        gameObject.SetActive(true);
+        moveDirection = (Vector2.zero - (Vector2)gameObject.transform.position).normalized;
+        StartCoroutine(ExecuteCoroutine());
+    }
+
+    private IEnumerator ExecuteCoroutine(){
+        while(true){
+            gameObject.transform.Translate(moveDirection * speed);
+            yield return YieldInstructionCache.WaitFrame;
+
+            if(gameObject.transform.position.x * moveDirection.x > -1){
+                ResetObject();
+            }
+        }       
+    }
+    
+    public void ResetObject(){
+        gameObject.SetActive(false);
+    }
+}
