54eeef5294ee56034e9e7d90b9a7d3828e8c8063
Komorio <twilivez@gmail.com>
2020-04-30 20:34:05 +0900

    [ADD] YieldInstructionCache 추가

---
+++ Assets/01.Scripts/System/YieldInstructionCache.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class YieldInstructionCache 
+{
+    private static Dictionary<float, WaitForSeconds> waitingSecond = new Dictionary<float, WaitForSeconds>();
+    private static object waitFrame = null;
+
+    public static object WaitFrame => waitFrame;
+
+    public WaitForSeconds WaitingSecond(float waitingTime){
+        if(!waitingSecond.ContainsKey(waitingTime)){
+            waitingSecond.Add(waitingTime, new WaitForSeconds(waitingTime));
+        }
+
+        return waitingSecond[waitingTime];
+    }
+
+}
