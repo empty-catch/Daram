4b6866ca3236ce11a5d42f4d12fc6ec18ec27a4c
박창용 <tempus157@gmail.com>
2020-05-01 16:22:19 +0900

    [UPDATE] Gesture UI 업데이트

--- Assets/01.Scripts/Gesture/Gesture.cs
+++ Assets/01.Scripts/Gesture/Gesture.cs
@@ -1,5 +1,34 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Gesture : MonoBehaviour
 {
+    public event Action<int[]> GestureDrawed;
+
+    private bool isDragging;
+    private Queue<int> numbers;
+
+    public void TouchBegan()
+    {
+        isDragging = true;
+    }
+
+    public void TouchEnded()
+    {
+        if (isDragging)
+        {
+            isDragging = false;
+            GestureDrawed?.Invoke(numbers.ToArray());
+            numbers.Clear();
+        }
+    }
+
+    public void TouchEntered(int index)
+    {
+        if (isDragging)
+        {
+            numbers.Enqueue(index);
+        }
+    }
 }
