766e71e701d203fb3ceff77da7f195672ff39732
Komorio <twilivez@gmail.com>
2020-05-01 22:15:47 +0900

    [UPDATE] Monster Key Image 보여주는 기능 추가

--- Assets/01.Scripts/Monster/Monster.cs
+++ Assets/01.Scripts/Monster/Monster.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using System;
 public class Monster : MonoBehaviour
 {
@@ -28,10 +29,22 @@ public class Monster : MonoBehaviour
     [SerializeField]
     private int score;
 
+    [SerializeField]
+    private GameObject[] keyObjects;
+    
+    private Image[][] keyImages;
+
+
     private void Awake(){
+        keyImages = new Image[keyObjects.Length][];
+
         monsterHp = defaultHp;
         monsterHpKeys = new int[monsterHp];
         spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+
+        for(int i = 0; i < keyObjects.Length; i++){
+            keyImages[i] = keyObjects[i].GetComponentsInChildren<Image>(true); 
+        }
     }
 
     public void SettingActions(Action<Monster> monsterGenerateAction, Action<Monster> monsterResetAction, Action<float> monsterAttackAction, Action<int> monsterDeathAction){
@@ -45,7 +58,7 @@ public class Monster : MonoBehaviour
         for(int i = 0; i < monsterHpKeys.Length; i++){
             if(monsterHpKeys[i].Equals(key)){
                 monsterHp--;
-
+                keyImages[monsterHpKeys[i]][0].gameObject.SetActive(false);
                 if(monsterHp <= 0){
                     Death();
                 }
@@ -61,12 +74,26 @@ public class Monster : MonoBehaviour
         monsterGenerateAction(this);
 
         for(int i = 0; i < monsterHpKeys.Length; i++){
-            monsterHpKeys[i] = UnityEngine.Random.Range(0,9);
+            monsterHpKeys[i] = UnityEngine.Random.Range(0,8);
+        }
+
+        Func<int, int> getAvailableKeyImage = (value) => {
+            for(int i = 0; i < keyImages.Length; i++){
+                if(keyImages[value][i].gameObject.activeInHierarchy.Equals(false)){
+                    return i;
+                }
+            }
+
+            return -1;
+        };
+
+        for(int i = 0; i < monsterHpKeys.Length; i++){
+            keyImages[monsterHpKeys[i]][getAvailableKeyImage(monsterHpKeys[i])].gameObject.SetActive(true);
         }
 
         moveDirection = (Vector2.zero - (Vector2)gameObject.transform.position).normalized;
         spriteRenderer.flipX = moveDirection.x > 0 ? true : false;
-        
+
         StartCoroutine(ExecuteCoroutine());
     }
 
@@ -75,7 +102,7 @@ public class Monster : MonoBehaviour
             gameObject.transform.Translate(moveDirection * speed);
             yield return YieldInstructionCache.WaitFrame;
 
-            if(gameObject.transform.position.x * moveDirection.x > -1){
+            if(gameObject.transform.position.x * moveDirection.x > -2){
                 Attack();
             }
         }       
@@ -93,6 +120,13 @@ public class Monster : MonoBehaviour
 
     public void ResetObject(){
         gameObject.SetActive(false);
+
+        for(int i = 0; i < keyImages.Length; i++){
+            for(int j = 0; j < keyImages.Length; j++){
+                keyImages[i][j].gameObject.SetActive(false);
+            }
+        }
+
         monsterResetAction(this);
         monsterHp = defaultHp;
     }
