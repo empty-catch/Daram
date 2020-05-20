400cae9387f4a10eb443ffd9611d6834ef5ace8a
Komorio <twilivez@gmail.com>
2020-05-20 13:53:51 +0900

    [UPDATE] TypeMonster 생성 방식 변경

--- Assets/01.Scripts/Monster/TypeMonster.cs
+++ Assets/01.Scripts/Monster/TypeMonster.cs
@@ -1,13 +1,56 @@
-﻿using System.Security.AccessControl;
+﻿using System;
+using System.Security.AccessControl;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TypeMonster : Monster
 {
+    [Header("Values")]
     [SerializeField]
     private Aura counterType;
     
+    private SpriteRenderer spriteRenderer;
+
+    [Header("Resources")]
+    [SerializeField]
+    private Sprite[] spriteResources;
+
+    private void Awake(){
+        base.BaseAwake();
+        
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    private void SetCounterType(){
+        var type = Enum.GetValues(typeof(Aura));
+        counterType = (Aura) type.GetValue(new System.Random().Next(type.Length));  
+
+        // FIXME : 변경
+        switch(counterType){
+            case Aura.Earth:
+            spriteRenderer.sprite = spriteResources[0];
+            break;
+
+            case Aura.Flame:
+            spriteRenderer.sprite = spriteResources[1];
+            break;
+            
+            case Aura.Ice:
+            spriteRenderer.sprite = spriteResources[2];
+            break;
+            
+            case Aura.Lightning:
+            spriteRenderer.sprite = spriteResources[3];
+            break;
+            
+            case Aura.Wind:
+            spriteRenderer.sprite = spriteResources[4];
+            break;
+
+        }
+    }
+
     public override void SetAuraFor(Aura aura, int level, float time){
         base.SetAuraFor(aura, level, time);
         if(counterType.Equals(aura)){
