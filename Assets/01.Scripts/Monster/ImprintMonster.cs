7d0c7649ba0903061906162853de4fab07a7018f
Komorio <twilivez@gmail.com>
2020-05-20 14:19:28 +0900

    [UPDATE] ImprintMonster 무작위로 스폰되게 변경

--- Assets/01.Scripts/Monster/ImprintMonster.cs
+++ Assets/01.Scripts/Monster/ImprintMonster.cs
@@ -5,14 +5,45 @@ using UnityEngine;
 
 public class ImprintMonster : Monster
 {
-    private enum gesture {vertical, up, horizontal, down, downArrow, upArrow, zigzag}
-
+    private enum gesture {vertical, up, horizontal, down, downArrow, upArrow}
+    
+    [Header("Values")]
     [SerializeField]
     private gesture sealGesture;
+
+    [Header("Resources")]
+    [SerializeField]
+    private Sprite[] spriteResources;
 
     private void SetRandomSealGesture(){
         var gestures = Enum.GetValues(typeof(gesture));
         sealGesture = (gesture) gestures.GetValue(new System.Random().Next(gestures.Length)); 
+
+        switch(sealGesture){
+            case gesture.vertical:
+            spriteRenderer.sprite = spriteResources[0];
+            break;
+
+            case gesture.up:
+            spriteRenderer.sprite = spriteResources[1];
+            break;
+
+            case gesture.horizontal:
+            spriteRenderer.sprite = spriteResources[2];
+            break;
+            
+            case gesture.down:
+            spriteRenderer.sprite = spriteResources[3];
+            break;
+            
+            case gesture.downArrow:
+            spriteRenderer.sprite = spriteResources[4];
+            break;
+
+            case gesture.upArrow:
+            spriteRenderer.sprite = spriteResources[5];
+            break;
+        }
     }
 
     public override void Execute(){
